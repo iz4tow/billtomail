3c45c0fe8e1dda8f180123a2ee608ef7e337abac
Franco Avino <f.avino@MELCHIONISPA.DOMAIN>
2017-11-27 08:55:21 +0100

    27/11/2017 - CORRETTA IMPORTAZIONE DA SYBASE IQ VERSO DB2

--- FATTURE ONLINE - STORICO DA SYBASE IQ A DB2/SYBASEIQ-DB2.py
+++ FATTURE ONLINE - STORICO DA SYBASE IQ A DB2/SYBASEIQ-DB2.py
@@ -5,6 +5,7 @@ import jpype
 import time
 import os 
 
+start_time = time.time()
 
 #CONNESSIONE A SYBASE IQ
 cnxn = pyodbc.connect("DSN=melc6h1_dmcomm")
@@ -22,13 +23,10 @@ curs=conn.cursor()
 #######BACKUP DATI SU DB2
 print ("INIZIO ESPORTAZIONE TABELLA DA DB2 PER BACKUP")
 oggi=time.strftime('%d-%m-%Y')#data di oggi
-curs.execute("SELECT * FROM DIGI.TABUTE_AZMAIL")
-rows=curs.fetchall()
 
 backup="AZMAIL-BACKUP"+oggi+".txt"
 
 if os.path.isfile(backup):
-	print ("ESISTE")
 	controllo1 = input("Esiste già un backup con la data di oggi, sovrascrivo? SI-NO ")
 	if controllo1=="SI" or controllo1=="si" or controllo1=="Si":
 		controllo2 = input("Sei sicuro? SI-NO")
@@ -40,7 +38,9 @@ if os.path.isfile(backup):
 		exit()
 
 	
-
+iniz_bck_time=time.time()
+curs.execute("SELECT * FROM DIGI.TABUTE_AZMAIL")
+rows=curs.fetchall()
 file = open(backup,"w") 
 for row in rows:
 	controllocampo1=0
@@ -55,7 +55,41 @@ for row in rows:
 	file.write ("\n")
 file.close()
 ########FINE BACKUP
+bck_time=time.time()
 
 
-	
 
+########PROCEDURA SYBASE -> DB2
+print ("INIZIO PROCEDURA IMPORTAZIONE DA SYBASE IQ VERSO DB2")
+cursiq.execute("SELECT codice_cliente,e_mail FROM DBA.clienti_email") #SELEZIONO TUTTA LA TABELLA
+rows=cursiq.fetchall()
+daimportare=len(rows)
+countmodificate=0
+countnuove=0
+for row in rows: #per ogni riga
+	codcli=row[0]
+	email=row[1]
+	curs.execute("SELECT DG_INDIRIZZO_EMAIL FROM DIGI.TABUTE_AZMAIL WHERE MK_CONTROPARTE='"+codcli+"' AND MK_TIPO_CONTROPART='CLIENT' AND MK_UNITA_ORGANIZ='CNTFOR'")
+	esisteibm=len(curs.fetchall())#lunghezza array estratto, conto le righe insomma...
+	if esisteibm>0: #SE GIA ESISTE AGGIORNO SOLO LA MAIL
+		curs.execute("UPDATE DIGI.TABUTE_AZMAIL SET DG_INDIRIZZO_EMAIL='"+email+"',MK_FATT_MAIL='1' WHERE MK_CONTROPARTE='"+codcli+"'")
+		curs.execute("commit")
+		countmodificate=countmodificate+1
+	else:#ALTRIMENTI LO INSERISCO
+		curs.execute("INSERT INTO DIGI.TABUTE_AZMAIL (DG_C_SOC,DG_C_DIVS,DG_C_VERS,MK_DIVISIONE,MK_TIPO_CONTROPART,MK_CONTROPARTE,MK_UNITA_ORGANIZ,MK_PROGR_2,DG_INDIRIZZO_EMAIL,MK_FATT_MAIL) VALUES ('0100','00','00','00','CLIENT','"+codcli+"','CNTFOR','1','"+email+"','1')")
+		curs.execute("commit")
+		countnuove=countnuove+1
+
+daimportare=str(daimportare)
+countnuove=str(countnuove)
+countmodificate=str(countmodificate)
+print ("\n\n\n\n")
+print ("RIGHE SU SYBASE IQ: "+daimportare)
+print ("RIGHE INSERITE SU DB2: "+countnuove)
+print ("RIGHE MODIFICATE SU DB2: "+countmodificate)
+
+end_time=time.time()
+print ("\n\n\n\n")
+print("---TEMPO REALE IMPIEGATO PER IL BACKUP: %s secondi ---" % (bck_time - iniz_bck_time))
+print("---TEMPO IMPIEGATO PER L'IMPORTAZIONE DA SYBASE IQ: %s secondi ---" % (end_time - bck_time))
+print("---TEMPO TOTALE DI ESECUZIONE: %s secondi ---" % (end_time - start_time))
